84ebdcc2b6aed14d3265d20c07dd66f42689459a
Founder of codedash.net <icttechincteam@gmail.com>
2020-06-09 03:05:16 +0000

    Added data save feature for login info

--- yUbuntuMain.java
+++ yUbuntuMain.java
@@ -4,10 +4,32 @@
 * Enjoy!
 */
 
+import java.util.Scanner;
+import java.io.*;
+
 public class yUbuntuMain {
 	public static void main (String[] args) {
 		System.out.println("Coming soon!");
 		System.out.println("Welcome to yUbuntu! Please wait for the terminal to be initialized...");
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Save login info? [yes/no]");
+		String saveLogin = sc.nextLine();
+		if (saveLogin.equals("yes")) {
+			createFile();
+		}
+	}
+
+	public static void createFile() {
+		try {
+			File loginDataF = new File("loginData.yUbuntu");
+			if (loginDataF.createNewFile()) {
+			  System.out.println("[  OK ] File created: " + loginDataF.getName());
+			} else {
+			  System.out.println("[ERROR ] File already exists.");
+			}
+		  } catch (IOException e) {
+			System.out.println("[ERROR ] An error occurred.");
+			e.printStackTrace();
+		}
 	}
 }
