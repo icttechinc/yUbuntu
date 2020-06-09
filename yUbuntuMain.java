29b5a1faf7d466194a88ebb30b47d4307ad2f378
icttechinc <65833301+icttechinc@users.noreply.github.com>
2020-06-08 22:23:53 -0400

    Created yUbuntuMain.java
    ~

---
+++ yUbuntuMain.java
@@ -0,0 +1,11 @@
+/**
+* yUbuntu
+* make sure to check out codedash.net (Learn code for free, forever.) and serverimage.xyz (a simple .iso mirror)!
+* Enjoy!
+*/
+
+public class yUbuntuMain {
+	public static void main (String[] args) {
+		System.out.println("Coming soon!");
+	}
+}
